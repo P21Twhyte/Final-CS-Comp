39e0d7c623f803a1ee5ecccbd26901ad33af3e93
Matthew Scully <p21mscully@fairfieldprep.org>
2021-05-11 12:50:37 -0400

    Test of Turtle

--- Matts_Part.py
+++ Matts_Part.py
@@ -1,1 +1,19 @@
 # Authors: MS 5/10/21
+
+import turtle
+
+window = turtle.Screen()
+rectangle = turtle.Turtle()
+
+window.setup(1000, 1000)
+window.title("Matching Game")
+
+rectangle.forward(250)
+rectangle.right(90)
+rectangle.forward(100)
+rectangle.right(90)
+rectangle.forward(250)
+rectangle.right(90)
+rectangle.forward(100)
+
+window.mainloop()
